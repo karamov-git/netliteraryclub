595d3d7bb0d1728c50fd1a9886cd05009c845346
karamov-git <ybiz177@gmail.com>
2020-06-28 13:34:56 +0500

    add refactoring task for chapter 5

---
+++ APPaP/APPap/Chapter5/GeneratePrimesSpec.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace Chapter5
+{
+    [TestFixture]
+    public class GeneratePrimesSpec
+    {
+        [Test]
+        public void TestPrimes()
+        {
+            int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+            Assert.AreEqual(nullArray.Length, 0);
+            int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+            Assert.AreEqual(minArray.Length, 1);
+            Assert.AreEqual(minArray[0], 2);
+            int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+
+            Assert.AreEqual(threeArray.Length, 2);
+            Assert.AreEqual(threeArray[0], 2);
+            Assert.AreEqual(threeArray[1], 3);
+            int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+            Assert.AreEqual(centArray.Length, 25);
+            Assert.AreEqual(centArray[24], 97);
+        }
+    }
+}
